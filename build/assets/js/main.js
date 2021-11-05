dc6fb4e84bd9e15694906cbd59014974fc087405
Vladimir <dopustimvladimir@gmail.com>
2021-11-06 00:34:14 +0600

    Run build task

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -7,9 +7,28 @@
             return false
         // console.log(`CoffeeScript ${CoffeeScript.VERSION} & CoffeeLint ${coffeelint.VERSION}`)
 
-        document.querySelector("#version").textContent = coffeelint.VERSION
-        document.querySelector("#year").textContent = new Date().getUTCFullYear()
+        const version = document.querySelector("#version")
 
+        if (version)
+            version.textContent = coffeelint.VERSION
+
+        const year = document.querySelector("#year")
+
+        if (year)
+            year.textContent = new Date().getUTCFullYear()
+
+        const rules = document.querySelector("#rules-table")
+
+        if (rules) {
+            rules.innerHTML+= "<tr><td>Rule name<td>Rule message"
+
+            const coffeelintRules = JSON.parse(JSON.stringify(coffeelint.getRules()))
+
+            for (const ruleName in coffeelintRules) {
+                const rule = coffeelintRules[ruleName]
+                rules.innerHTML+= `<tr><td>${rule.name}<td>${rule.message}`
+            }
+        }
         return true
     })
 
